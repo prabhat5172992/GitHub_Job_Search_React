2dbb6e236b56050354ab9966714b93f5d3eca20a
prabhat5172992 <prabhat5172992@gmail.com>
2020-08-04 20:39:11 +0530

    first commit

---
+++ src/useFetchJobs.js
@@ -0,0 +1,55 @@
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
+
+const ACTIONS = {
+    MAKE_REQUEST: 'make_request',
+    GET_DATA: 'get_data',
+    ERROR: 'error',
+    UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
+}
+
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
+
+function reducer(state, action) {
+    switch(action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loadinbg: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: [] }
+        case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+            return { ...state, hasNextPage: action.payload.hasNextPage}
+        default:
+            return state;
+    }
+}
+
+export default function useFetchJobs (params, page) {
+    const [state, dispatch]  = useReducer(reducer, { jobs: [], loading: true });
+
+    useEffect(() => {
+        const cancelToken = axios.CancelToken.source()
+        dispatch({ type: ACTIONS.MAKE_REQUEST })
+        axios.get(BASE_URL, {
+            cancelToken: cancelToken.token,
+            params: { markdown: true, page: page, ...params },
+            headers: {
+                // "Content-Type": "application/json",
+                "X-Requested-With": "XMLHttpRequest",
+                //"Accept": "*/*",
+            },
+        }).then(res => {
+            dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data }})
+        }).catch(e => {
+            if (axios.isCancel(e)) return;
+            dispatch({ type: ACTIONS.ERROR, payload: {error: e} })
+        }) 
+
+        return () => {
+            cancelToken.cancel();
+        }
+    }, [params, page]);
+
+    return state
+}
